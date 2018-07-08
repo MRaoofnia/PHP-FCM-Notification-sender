c087a058d195c67e5908c2035bd605cfb55b36f2
mohammad RaoofNia <mraoofnia@gmail.com>
2018-07-08 19:54:10 +0430

    config.php updated

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-  define ('FIREBASE_API_KEY','AAAAw-RazEQ:APA91bFFMwHc0EqQG2byOT296g3aHO6AIG9y9AUKHodK5Sw6DMd0fhDHiW2TI0NgIgAiUe9eIIplZuLms8VZn7nmGfAbtExRbiRw6DuSDA-p0plrR93UEoQIzWuwl7300PsUhn0Uwp4xEu0usDUl3TguPFFk51v96g'); //get your key from Firebase console > Project settings > Cloud messaging tab > Server key
+  define ('FIREBASE_API_KEY','your key here'); //get your key from Firebase console > Project settings > Cloud messaging tab > Server key
  ?>
